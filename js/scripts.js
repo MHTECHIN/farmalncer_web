8c71b7f07b57a73821f958735fdc8e7fce0f3e06
Adinath Nage <adinathnage9739@gmail.com>
2025-01-27 17:30:55 +0530

    Updated inline JavaScript for tab content switching

--- js/scripts.js
+++ js/scripts.js
@@ -256,7 +256,7 @@
     // create the back to top button
     $('body').prepend('<a href="body" class="back-to-top page-scroll">Back to Top</a>');
     var amountScrolled = 700;
-    $(window).scroll(function() {
+    $(window).scroll(function() { 
         if ($(window).scrollTop() > amountScrolled) {
             $('a.back-to-top').fadeIn('500');
         } else {
